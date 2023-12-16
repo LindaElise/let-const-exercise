47c0e9cca82319693f52b846e8a3bb53b9e1f718
Linda Sherman <eliseest1900@gmail.com>
2023-12-15 19:53:44 -0500

    Adding all files

---
+++ let-const-exercise.js
@@ -0,0 +1,22 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+const PI = 3.14;
+PI = 4.15 //Not allow
+
+//What is the difference between var and let?
+//var allows you to redeclare and reassing values in the same keyword. var can have block scope.
+//let allows you to reassing values but not redeclare the same keyword.
+
+
+//What is the difference between var and const?
+//var allows you to redeclare and reassign values in a keyword.
+//const does not allow to reassing or redeclare the same keyword. const creates block scope.
+
+//What is the difference between let and const?
+//let allows to reassing values in a keyword. 
+//const does not allow using the same keyword to reassign or redeclare.
+
+//What is hoisting?
+//It's the way JavaScript compiter works.
